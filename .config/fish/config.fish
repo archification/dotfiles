2a51ea0e1cf3c73210325d74a7b33f18958a9181
John Guillera <archification@gmail.com>
2018-06-04 21:51:08 -0500

    more stuff

---
+++ .config/fish/config.fish
@@ -0,0 +1,10 @@
+# ~/.config/fish/config.fish
+
+fish_vi_key_bindings
+set fish_plugins agnoster
+
+set -g theme_nerd_fonts yes
+
+alias pacaur='aurman'
+alias nano='vim'
+alias config='git --git-dir=$HOME/.cfg/ --work-tree=$HOME'
