2f6643be1ab554c09491bf6bf7a1d99f4547b322
archification <archification@gmail.com>
2018-12-04 02:54:54 -0600

    scripted commit

---
+++ .config/scripts/lock.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+IMAGE=/tmp/i3lock.png
+SCREENSHOT="scrot $IMAGE"
+BLURTYPE="2x8" # 2.90s
+$SCREENSHOT
+convert $IMAGE -blur $BLURTYPE $IMAGE
+i3lock -i $IMAGE
+rm $IMAGE
--- git.fish
+++ git.fish
@@ -12,23 +12,29 @@ config add compton.conf
 
 cd fish
 config add config.fish
-
 cd ..
+
 cd i3
 config add config
-
 cd ..
+
 cd scripts
 config add disk
 config add load_average
+config add lock.sh
 config add battle.sh
 config add macro.sh
 config add path.sh
 config add pob.sh
-
 cd ..
+
 cd kitty
 config add kitty.conf
+cd ..
+
+cd termite
+config add config
+cd ..
 
 config commit -m 'scripted commit'
 config push -u origin master
