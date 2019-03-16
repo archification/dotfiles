dd655c4e618e26837fca03509db0fd564239c576
archification <archification@gmail.com>
2019-03-16 14:28:37 -0500

    scripted commit

--- .config/scripts/pastefile.sh
+++ .config/scripts/pastefile.sh
@@ -8,15 +8,6 @@ then
 	echo "\$0: File '${1}' not found." && exit
 fi
 
-extension=$(echo $1 | sed 's/^\w\+.//')
-if [ "$extension" == "sh" ];
-then
-	filetype=bash
-elif [ "$extension" == "py" ];
-then
-	filetype=python
-else
-	filetype=text
-fi
+filetype=$(sed 's/^.*\.//' <<< $1)
 { echo \`\`\`$filetype && cat $1 && echo \`\`\`; } | xsel -ib
 mpv "$HOME/.config/sounds/thing.mp3" &>/dev/null &
