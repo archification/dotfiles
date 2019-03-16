301dfbc31b203f41133febecedd40ac724e25541
archification <archification@gmail.com>
2019-03-16 00:36:08 -0500

    scripted commit

--- .config/scripts/pastefile.sh
+++ .config/scripts/pastefile.sh
@@ -1,6 +1,14 @@
 #!/bin/bash
-read filename
-extension=$(echo $filename | sed 's/^\w\+.//')
+
+if [ -z "$1" ]
+then
+	echo "usage: $0 [ filename ]" && exit
+elif [ ! -f "$1" ]
+then
+	echo "\$0: File '${1}' not found." && exit
+fi
+
+extension=$(echo $1 | sed 's/^\w\+.//')
 if [ "$extension" == "sh" ];
 then
 	filetype=bash
@@ -10,5 +18,5 @@ then
 else
 	filetype=text
 fi
-{ echo \`\`\`$filetype && cat $filename && echo \`\`\`; } | xsel -ib
+{ echo \`\`\`$filetype && cat $1 && echo \`\`\`; } | xsel -ib
 mpv "$HOME/.config/sounds/thing.mp3" &>/dev/null &
