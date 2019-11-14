174d0096c5cb7d6b50c54d8f66465419c1941f30
archification <archification@gmail.com>
2019-11-14 13:02:29 -0600

    scripted commit

--- .config/scripts/update.sh
+++ .config/scripts/update.sh
@@ -4,4 +4,4 @@ yay -Syu
 dotgit.sh
 cd $HOME/.config/nvim/bundle
 find . -maxdepth 1 -name .git -type d | rev | cut -c 6- | rev | xargs -I {} git -C {} pull
-mpv "$HOME/.config/sounds/thing.mp3"
+mpv "$HOME/.config/sounds/upgradecomplete.mp3"
