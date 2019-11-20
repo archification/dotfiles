ffb4452bd9305fa44f1534dadb4accf7cf78414b
archification <archification@gmail.com>
2019-11-20 13:48:37 -0600

    scripted commit

--- .local/bin/updates/update.sh
+++ .local/bin/updates/update.sh
@@ -4,5 +4,5 @@ yay -Syu
 dotgit.sh
 cd $HOME/.config/nvim/bundle
 find . -maxdepth 1 -name .git -type d | rev | cut -c 6- | rev | xargs -I {} git -C {} pull
-mpv --really-quiet "upgradecomplete.mp3" &
+mpv --really-quiet "$HOME/.local/bin/sounds/upgradecomplete.mp3" &
 disown && exit
