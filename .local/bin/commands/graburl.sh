129249a74a50e95c418109fe57a41a49b150a234
archification <archification@gmail.com>
2019-12-26 02:02:22 -0600

    scripted commit

--- .local/bin/commands/graburl.sh
+++ .local/bin/commands/graburl.sh
@@ -1,4 +1,11 @@
 #!/bin/env sh
-maim -s | imgur.sh && xclip -o | xsel -ib
-mpv "$HOME/.local/bin/sounds/thing.mp3" &
+
+maim -s -l -c 0,0.5,0.5,0.3 -u --format png /dev/stdout \
+    | curl -F "file=@-" 0x0.st \
+    | xclip -selection clipboard
+notify-send -u low \
+    "screenshot" \
+    "it's done" \
+    -i $HOME/pictures/avatars/arch.png
+mpv --volume 50 --really-quiet "$HOME/.local/bin/sounds/thing.mp3" &
 disown && exit
--- .local/bin/updates/update.sh
+++ .local/bin/updates/update.sh
@@ -3,6 +3,10 @@
 yay -Syu
 dotgit.sh
 cd $HOME/.config/nvim/bundle
-find . -maxdepth 1 -name .git -type d | rev | cut -c 6- | rev | xargs -I {} git -C {} pull
-mpv --really-quiet "$HOME/.local/bin/sounds/upgradecomplete.mp3" &
+find . -maxdepth 1 -name .git -type d \
+    | rev \
+    | cut -c 6- \
+    | rev \
+    | xargs -I {} git -C {} pull
+mpv --volume 50 --really-quiet "$HOME/.local/bin/sounds/upgradecomplete.mp3" &
 disown && exit
