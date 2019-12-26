c23c435ad12d8bda27df852aa9a35b012038a680
archification <archification@gmail.com>
2019-12-26 02:16:35 -0600

    scripted commit

--- .local/bin/updates/update.sh
+++ .local/bin/updates/update.sh
@@ -8,5 +8,9 @@ find . -maxdepth 1 -name .git -type d \
     | cut -c 6- \
     | rev \
     | xargs -I {} git -C {} pull
+notify-send -u low \
+    "updates" \
+    "They're done." \
+    -i $HOME/pictures/avatars/arch.png
 mpv --volume 50 --really-quiet "$HOME/.local/bin/sounds/upgradecomplete.mp3" &
 disown && exit
