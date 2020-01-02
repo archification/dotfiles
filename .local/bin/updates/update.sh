f2b13bacf19e6bc9ce046880f6829e758baf2800
archification <archification@gmail.com>
2020-01-02 01:13:39 -0600

    scripted commit

--- .local/bin/updates/update.sh
+++ .local/bin/updates/update.sh
@@ -3,8 +3,15 @@
 yay -Syu
 dotgit.sh
 rustup update
+
 cd $HOME/.config/nvim/bundle
-find . -maxdepth 1 -type d -print -execdir git --git-dir={}/.git --work-tree=$PWD/{} pull origin master \;
+dirs=`ls -1d */`
+for d in $dirs
+do
+    echo `cd $d; git pull`
+done
+
+#find . -maxdepth 1 -type d -print -execdir git --git-dir={}/.git --work-tree=$PWD/{} pull origin master \;
 #find . -maxdepth 1 -name .git -type d \
 #    | rev \
 #    | cut -c 6- \
