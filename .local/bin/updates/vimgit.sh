f6fc65e092a5821baaceb0d00d5cdfe7b93c4b6f
archification <archification@gmail.com>
2020-01-02 01:24:50 -0600

    scripted commit

--- .local/bin/updates/vimgit.sh
+++ .local/bin/updates/vimgit.sh
@@ -5,10 +5,10 @@ dirs=`ls -1d */`
 for d in $dirs
 do
     echo $d
-    echo '======================'
     IFS=$'\n' lines=($(cd $d; git pull))
     for l in $lines
     do
         echo $l
     done
+    echo '======================'
 done
