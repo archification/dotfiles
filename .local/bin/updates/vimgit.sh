12717193320099ba4d916cb7146aa8e5ea1c1f48
archification <archification@gmail.com>
2020-07-17 12:03:50 -0500

    scripted commit

--- .local/bin/updates/vimgit.sh
+++ .local/bin/updates/vimgit.sh
@@ -7,7 +7,7 @@ dirs=`ls -1d */`
 for d in $dirs
 do
     echo $d
-    IFS=$'\n' lines=($(cd $d; git config --get remote.origin.url; git pull))
+    IFS=$'\n' lines=($(cd $d; git config pull.rebase false; git config --get remote.origin.url; git pull))
     for l in $lines
     do
         echo $l
