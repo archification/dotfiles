d5cc21574ac6267155c22d1dcf14a09e3d32b580
archification <archification@gmail.com>
2024-07-22 20:16:56 -0400

    scripted commit

--- .local/bin/updates/update.sh
+++ .local/bin/updates/update.sh
@@ -7,6 +7,6 @@ rustup update
 notify-send -u low \
     "updates" \
     "They're done." \
-    -i $HOME/pictures/avatars/arch.png
+    -i $HOME/pictures/avatars/arch.webp
 mpv --volume=50 --really-quiet "$HOME/.local/bin/sounds/upgradecomplete.mp3" &
 disown && exit
