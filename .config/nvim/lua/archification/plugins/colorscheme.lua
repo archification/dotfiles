2ef4ff4b0bb3e46a1f0f64e350f0252280105084
archification <archification@gmail.com>
2024-08-20 23:42:42 -0400

    scripted commit

--- .config/nvim/lua/archification/plugins/colorscheme.lua
+++ .config/nvim/lua/archification/plugins/colorscheme.lua
@@ -4,6 +4,9 @@ return {
     {
         "svrana/neosolarized.nvim",
         config = function()
+            require("neosolarized").setup({
+                background_set = vim.g.neovide,
+            })
             vim.cmd([[colorscheme neosolarized]])
         end,
     },
